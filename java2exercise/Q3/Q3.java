d0a609f94cce50527669fe19f4aa16c0d4862efe
khuranaTTN <60733978+khuranaTTN@users.noreply.github.com>
2020-02-20 15:35:29 +0530

    Exercise of java2

--- java2exercise/Q3/Q3.java
+++ java2exercise/Q3/Q3.java
@@ -2,13 +2,10 @@ package Q3;
 // WAP to produce NoClassDefFoundError and ClassNotFoundException exception.
 public class Q3 {
     public static void main(String[] args) {
-
-       /*try{
-            obj o = new obj();
-        }
-        catch (NoClassDefFoundError e){
-            System.out.println(e);
-        }*/
+         try{
+            Class.forName("Chirag");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getStackTrace());
     }
 }
-
+}
